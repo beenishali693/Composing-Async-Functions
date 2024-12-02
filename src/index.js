d305d118f862b699a8c04d501bc5788d410e98de
Beenish <beenishali693@gmail.com>
2024-12-02 12:20:27 -0800

    one_solution

--- src/index.js
+++ src/index.js
@@ -11,9 +11,19 @@ const getRandomValue = (values) => {
 // returns a Promise to an array of breed names
 
 const getBreeds = () => {
-  // implement me!
+  return axios
+    .get('https://dog.ceo/api/breeds/list/all')
+    .then((response) => {
+      return Object.keys(response.data.message);
+    })
+    .catch(() => {
+      console.log('error!');
+    });
 };
-
+// getBreeds()
+//   .then(breeds => {
+//     console.log(breeds);
+//   });
 // Helper method to retrieve a random image for a
 // specified breed
 // https://dog.ceo/dog-api/documentation/breed
@@ -21,16 +31,37 @@ const getBreeds = () => {
 // returns a Promise to a url (string)
 
 const getRandomImageForBreed = (breed) => {
-  // implement me!
+  return axios
+    .get(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then((response) => {
+      return response.data.message;
+    })
+    .catch(() => {
+      console.log('error!');
+    });
 };
+
+// getRandomImageForBreed('husky')
+//   .then(imageURL => {
+//     console.log(imageURL);
+//   });
+
 
 // use our other helpers to make a function that returns
 // a random image from a random breed
 // returns a Promise to a url (string)
 
 const getRandomDogImage = () => {
-  // implement me!
+  return getBreeds()
+    .then((breeds) => {
+      const randomBreed = getRandomValue(breeds);// Call getRandomValue properly
+      return getRandomImageForBreed(randomBreed); // Fetch a random image for the breed
+    })
+    .catch((error) => {
+      console.error('Error fetching random dog image:', error);
+    });
 };
+
 
 // This is the call we would like to make work
 // This function should return a Promise to a url (string)
